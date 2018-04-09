5581cf15cf7e253759b155ab96837e007205b139
sarathkaleswaram <ksb994@gmail.com>
2018-04-09 19:55:55 +0530

    Desktop Notification

--- src/app/providers/notification.service.ts
+++ src/app/providers/notification.service.ts
@@ -17,32 +17,33 @@ export enum SeverityType {
 
 export interface NotificationType {
     severity?: SeverityType;
-    title: string; 
+    title: string;
     body: string;
 }
 
 @Injectable()
 export class NotificationService {
-    
+
     private messagesSubject = new Subject<NotificationType>();
     private nativeNotificationSupported: boolean;
- 
+
     constructor(private router: Router,
-                private logger: LogService,
-                private electronService: ElectronService,
-                private messageService: MessageService) {
+        private logger: LogService,
+        private electronService: ElectronService,
+        private messageService: MessageService) {
         this.logger.debug("### INIT NotificationService");
         this.nativeNotificationSupported = this.electronService.remote.Notification.isSupported();
         this.logger.debug("### NotificationService - Native Support?: " + this.nativeNotificationSupported);
     }
 
-    addMessage(msg: NotificationType){
+    addMessage(msg: NotificationType) {
         this.logger.debug("### NotificationService: " + JSON.stringify(msg));
-        let notificationOptions: NotificationOptions = {
+        this.electronService.ipcRenderer.send('push-notification', msg);
+        /* let notificationOptions: NotificationOptions = {
             tag: "CasinoCoin",
             icon: path.join(__dirname, 'assets/brand/casinocoin-icon-256x256.png'),
             body: msg.body
         }
-        let notification = new Notification(msg.title, notificationOptions);
+        let notification = new Notification(msg.title, notificationOptions); */
     }
 }
